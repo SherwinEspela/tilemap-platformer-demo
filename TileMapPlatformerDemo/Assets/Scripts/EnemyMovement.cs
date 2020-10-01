f8a3bfbfb01fe321b68deb1277d0cf3c3e7bda52
Sherwin Espela <sherwin.espela@gmail.com>
2020-09-30 19:03:18 -0700

    [develop] * adding Enemy wip

---
+++ TileMapPlatformerDemo/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D _rigidBody;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+}
